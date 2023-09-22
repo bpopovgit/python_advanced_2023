08dbf7005831ce66f95ee509dc342af7723d3836
Boris Popov <32306686+bpopovgit@users.noreply.github.com>
2023-09-22 15:12:02 +0300

    add exercise tasks

---
+++ stacks_queues_tuples_and_sets_exercises/02_expression_evaluator.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+expression = input().split()
+numbers = deque()
+
+operators = {
+    "+": lambda a, b: a + b,
+    "-": lambda a, b: a - b,
+    "*": lambda a, b: a * b,
+    "/": lambda a, b: a // b,
+}
+
+for char in expression:
+    if char not in '+-*/':
+        numbers.append(int(char))
+    else:
+        while len(numbers) > 1:
+            first_num = numbers.popleft()
+            second_num = numbers.popleft()
+            # if char == "+":
+            #     numbers.appendleft(first_num + second_num)
+            # elif char == "-":
+            #     numbers.appendleft(first_num - second_num)
+            # elif char == "*":
+            #     numbers.appendleft(first_num * second_num)
+            # elif char == '/':
+            #     numbers.appendleft(first_num // second_num)
+            numbers.appendleft(operators[char](first_num, second_num))
+
+print(numbers[0])
