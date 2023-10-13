4383829a7f3f093ee22cabaac9b280e1883757db
Boris Popov <32306686+bpopovgit@users.noreply.github.com>
2023-10-13 18:04:52 +0300

    add additional checks to tic tac toe console game

--- workshop_02/01_console_tic_tac_toe.py
+++ workshop_02/01_console_tic_tac_toe.py
@@ -5,24 +5,24 @@ def is_valid_sign(player_one_sign_):
 
 
 def render_board(board_):
-    for row in board_:
+    for row_ in board_:
         print("| ", end="")
-        print(" | ".join([sign if sign else " " for sign in row]), end="")
+        print(" | ".join([sign if sign else " " for sign in row_]), end="")
         print(" |")
 
 
 def is_row_win(board_):
-    for row in board_:
-        if len(set(row)) == 1 and set(row) != {None}:
+    for row_ in board_:
+        if len(set(row_)) == 1 and set(row_) != {None}:
             return True
     return False
 
 
 def is_column_win(board_, current_sign_):
-    for col in range(len(board_)):
+    for col_ in range(len(board_)):
         current_col = []
         for row in range(len(board_)):
-            current_col.append(board_[row][col] == current_sign_)
+            current_col.append(board_[row][col_] == current_sign_)
         if all(current_col):
             return True
     return False
@@ -42,8 +42,36 @@ def is_win(board_, current_sign_):
     return False
 
 
+def is_row_win_possible(board_):
+    if all(['X' in row_ and '0' in row_ for row_ in board_]):
+        return False
+    return True
+
+
+def is_col_win_possible(board_):
+    columns = []
+    for col_ in range(len(board_)):
+        current_column = []
+        for row_ in range(len(board_)):
+            current_column.append(board_[row_][col_])
+        columns.append(current_column)
+    if all(['X' in col_ and '0' in col_ for col_ in columns]):
+        return False
+    return True
+
+
+def is_diagonal_win_possible(board_):
+    diagonal_1, diagonal_2 = [], []
+    for inx in range(len(board_)):
+        diagonal_1.append(board_[inx][inx])
+        diagonal_2.append(board_[inx][len(board_) - 1 - inx])
+    if 'X' in diagonal_1 and '0' in diagonal_1 and 'X' in diagonal_2 and '0' in diagonal_2:
+        return False
+    return True
+
+
 def is_draw(board_):
-    if any([None in row for row in board_]):
+    if any([is_row_win_possible(board_), is_col_win_possible(board_), is_diagonal_win_possible(board_)]):
         return False
     return True
 
@@ -56,7 +84,7 @@ def is_valid_choice(board_, board_mapper_, choice_):
         return False
     if board_[board_mapper_[choice_][0]][board_mapper_[choice_][1]]:
         return False
-    return  True
+    return True
 
 
 player_one = input("Player one name: ").strip()
